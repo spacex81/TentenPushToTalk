bad31bd15761af6c617f473aef2d9e77d17102e8
YK <spacex81@gmail.com>
2024-06-21 17:36:23 +0900

    d

---
+++ App/App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App
+//
+//  Created by jo on 6/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Push to Talk")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
